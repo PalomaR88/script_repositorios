23a5f20b3d359b66a432ff2159c108e1a4782bfa
Paloma R. García Campón <paloma@coatlicue>
2019-10-02 13:01:08 +0200

    subido

---
+++ script_repositorios.sh
@@ -0,0 +1,26 @@
+#! bin/sh
+
+#Script que introduciendo un repositorio como argumento devuelve los paquetes que han se han descargado de dicho repositorio o sus dependencias.
+
+echo "Buscando paquetes:"
+
+#Recorre la lista de paquetes
+for i in `dpkg -l | grep '^ii' | cut -d " " -f 3`
+do
+	#Repositorios necesarios para cada paquete
+	repositorios=$(apt-cache policy $i | sed -n /*/,/100/p | egrep "http*" | awk '{print $2}')
+
+	#Recorre la lista de repositorios de un paquete concreto ($i)
+	for x in $repositorios
+	do	
+
+		if [ $1 = $x ]
+		then
+			echo "$i"
+		fi
+
+	done
+done
+
+
+
